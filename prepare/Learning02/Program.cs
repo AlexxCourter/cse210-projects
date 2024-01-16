9500dd12c0985b74c42cd43089556c004bf16e32
AlexxCourter <alex.courter@gmail.com>
2024-01-16 14:45:59 -0500

    completed learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        // Console.WriteLine(job1._company);
+
+        Job job2 = new();
+
+        job2._jobTitle = "Senior Software Engineer";
+        job2._company = "Apple";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        // Console.WriteLine(job2._company);
+
+        Resume resume1 = new();
+
+        resume1._name = "John Doe";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        // Console.WriteLine(resume1._jobs[0]._jobTitle);
+        resume1.Display();
     }
 }
