84612dacdf2f6f52b8aae8f20c30e097f4276a31
AlexxCourter <alex.courter@gmail.com>
2024-01-30 16:01:51 -0500

    completed learning04 inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assign = new("Alex", "Math");
+        Console.WriteLine(assign.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment math = new("Alexander", "Math", "101", "8-10");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment write = new("Alexx", "English", "Analysis of Shakespeare");
+        Console.WriteLine(write.GetSummary());
+        Console.WriteLine(write.GetWritingInformation());
+
     }
 }
