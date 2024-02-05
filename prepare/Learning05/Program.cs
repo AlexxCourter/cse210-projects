914e518aeca9b16c28e7fc8e04d7ab460903d741
AlexxCourter <alex.courter@gmail.com>
2024-02-05 16:46:01 -0500

    completed Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        //call iterations of the shape classes
+        Square sq = new("red", 5.5);
+        Rectangle rec = new("blue", 2, 4);
+        Circle circ = new("green", 6);
+
+        List<Shape> shapes = new(){
+            sq,
+            rec,
+            circ
+        };
+
+        foreach(Shape shape in shapes)
+        {
+        Console.WriteLine(shape.GetColor());
+        Console.WriteLine(shape.GetArea());
+        Console.WriteLine(); //spacer
+        }
     }
 }
